ea1a9cd3e13d90f51e8153d660060e2d0e0e49a6
Krish Arora <krisharora27@gmail.com>
2021-06-07 20:56:01 -0400

    Fixed bugs in python script

--- fonts/rename.py
+++ fonts/rename.py
@@ -2,7 +2,7 @@
 # To use this script, set the color to the color of the font in lowercase, and set the fntSize and fntName variables
 # Unzip the .zip file from ttf2fnt.com and place its contents here
 # All of the .png files, the fnt file, and the references to the pngs will automatically be renamed, assuming you set the variables correctly
-# Note this script does NOT account for fnt fonts with 10 or more png files, as many variables are hard coded
+# Note this script does NOT account for fnt fonts with 10 or more png files, as many variables are hard-coded
 
 import os
 
@@ -11,12 +11,11 @@ color = "black"
 fntSize = 128
 fntName = "Roboto-Regular"
 for filename in os.listdir(PATH):
-    if filename.startswith(fntName + ".ttf") and not filename == (fntName + ".ttf.fnt" or fntName + ".ttf"):
+    if filename.startswith(fntName + ".ttf_"):
         num = filename[-5]
         newFileName = fntName + "_" + color + str(fntSize) + "_" + num + ".png"
         org_fp = os.path.join(PATH, filename)
         new_fp = os.path.join(PATH, newFileName);
-        print(filename + "\n" + newFileName + "\n\n")
         os.rename(org_fp, new_fp)
 try:
     with open(os.path.join(PATH, fntName + ".ttf.fnt"), 'r') as f:
@@ -30,4 +29,4 @@ try:
     fntFileName = fntName + "_" + color + str(fntSize) + ".fnt"
     os.rename(os.path.join(PATH, fntName + ".ttf.fnt"), os.path.join(PATH, fntFileName))
 except:
-    print("File already renamed")
+    pass
