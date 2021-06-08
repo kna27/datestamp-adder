08603dc6951bea4aee9f0deb280e1f33b56af99a
Krish Arora <krisharora27@gmail.com>
2021-06-08 17:45:01 -0400

    Python script now accounts for up to 99 .pngs

--- fonts/rename.py
+++ fonts/rename.py
@@ -2,7 +2,6 @@
 # To use this script, set the color to the color of the font in lowercase, and set the fntSize and fntName variables
 # Unzip the .zip file from ttf2fnt.com and place its contents here
 # All of the .png files, the fnt file, and the references to the pngs will automatically be renamed, assuming you set the variables correctly
-# Note this script does NOT account for fnt fonts with 10 or more png files, as many variables are hard-coded
 
 import os
 
@@ -13,19 +12,21 @@ color = "black"
 fntSize = 128
 fntName = "Roboto-Regular"
 
+i = 0
 for filename in os.listdir(PATH):
     if filename.startswith(fntName + ".ttf_"):
-        num = filename[-5]
-        newFileName = fntName + "_" + color + str(fntSize) + "_" + num + ".png"
+        newFileName = fntName + "_" + color + str(fntSize) + "_" + str(i) + ".png"
         org_fp = os.path.join(PATH, filename)
-        new_fp = os.path.join(PATH, newFileName);
+        new_fp = os.path.join(PATH, newFileName)
         os.rename(org_fp, new_fp)
+        i += 1
 try:
     with open(os.path.join(PATH, fntName + ".ttf.fnt"), 'r') as f:
         lines = f.readlines()
     for line in range(len(lines)):
         if lines[line].startswith("page"):
-            newLine = "page id=" + lines[line][8] + " file=\"" + fntName + "_" + color + str(fntSize) + "_" + lines[line][8] + ".png\"\n"
+            id = str(int(lines[line][8:10]))
+            newLine = "page id=" + id + " file=\"" + fntName + "_" + color + str(fntSize) + "_" + id + ".png\"\n"
             lines[line] = newLine
     with open(os.path.join(PATH, fntName + ".ttf.fnt"), 'w') as f:
         f.writelines(lines)
