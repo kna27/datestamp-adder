bbb6f177368f182b8556785969158e47429f1cbf
Krish Arora <krisharora27@gmail.com>
2021-06-09 12:45:52 -0400

    Added date format to session storage

--- public/formStorage.js
+++ public/formStorage.js
@@ -2,6 +2,7 @@ const submitBtn = document.getElementById("submit");
 const posBtns = document.getElementsByName("pos");
 const fontColor = document.getElementById("color");
 const fontSize = document.getElementById("fontsize");
+const dateFormat = document.getElementById("dateformat");
 
 submitBtn.onclick = function () {
     for (i = 0; i < posBtns.length; i++) {
@@ -11,7 +12,10 @@ submitBtn.onclick = function () {
     }
     sessionStorage.setItem("color", fontColor.value);
     sessionStorage.setItem("size", fontSize.value);
+    sessionStorage.setItem("dateformat", dateFormat.value);
 }
+
 document.getElementById(sessionStorage.getItem("pos") == null ? "br" : sessionStorage.getItem("pos")).checked = true;
 fontColor.value = sessionStorage.getItem("color") == null ? "orange" : sessionStorage.getItem("color");
 fontSize.value = sessionStorage.getItem("size") == null ? "64" : sessionStorage.getItem("size");
+dateFormat.value = sessionStorage.getItem("dateformat") == null ? "dateandtime12" : sessionStorage.getItem("dateformat");
